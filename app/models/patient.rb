03481e6e594a1346f60be1285ada9722f3c6bf01
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-04-11 13:52:04 -0300

    CPF validation

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,18 +1,16 @@
 class Patient < ApplicationRecord
-    validates :name, :birth_date, :cpf, presence: true
+    validates :name, :birth_date, presence: true
     validates :cpf, uniqueness: true
 
     validates_cpf_format_of :cpf
 
-    before_save do
-        formatCPF
-    end
+    before_save :formatCPF
 
     private
 
     def formatCPF
-        value = CPF.new(cpf)
-        self.cpf = value.formatted
-        value.valid?
+        # value = CPF.new(cpf)
+        self.cpf.formatted
+        self.cpf.valid?
     end
 end
