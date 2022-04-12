63d145ad1c7b0d176906e1d90f586f9a525c6392
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-04-12 14:40:59 -0300

    Cannot add duplicate CPF

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,5 +3,20 @@ class Appointment < ApplicationRecord
     belongs_to :doctor
     validates :patient_id, :doctor_id, :starts_at, :ends_at, presence: true
 
+    # before_validation :is_opening_hours?
+
+    # private
     
+    # def is_opening_hours?
+    #     start_time = starts_at.strftime('%H:%M')
+    #     end_time = ends_at.strftime('%H:%M')
+
+    #     if (start_time < '09:00' || start_time > '18:00') && (end_time >= '18:01')
+    #         errors.add(:base, 'Out of opening hours')
+    #         throw :abort
+    #     elsif ('12:00..12:59').cover?(start_time)
+    #         errors.add(:base, 'Out of opening hours')
+    #         throw :abort
+    #     end
+    # end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,16 +1,16 @@
 class Patient < ApplicationRecord
-    validates :name, :birth_date, presence: true
+    validates :name, :birth_date, :cpf, presence: true
     validates :cpf, uniqueness: true
 
     validates_cpf_format_of :cpf
 
-    before_save :formatCPF
+    before_validation :formatCPF
 
     private
 
     def formatCPF
-        # value = CPF.new(cpf)
-        self.cpf.formatted
-        self.cpf.valid?
+        value = CPF.new(cpf)
+        self.cpf = value.formatted
+        value.valid?
     end
 end
