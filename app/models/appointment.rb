5c2d06cf2e8b3e4748d09bef444d6f20e274d68b
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-04-13 15:08:40 -0300

    Check if date and time for appointment is valid

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,20 +3,16 @@ class Appointment < ApplicationRecord
     belongs_to :doctor
     validates :patient_id, :doctor_id, :starts_at, :ends_at, presence: true
 
-    # before_validation :is_opening_hours?
+    before_validation do
+        is_opening_hours
+        throw(:abort) if errors.present?
+    end
 
-    # private
-    
-    # def is_opening_hours?
-    #     start_time = starts_at.strftime('%H:%M')
-    #     end_time = ends_at.strftime('%H:%M')
+    private
 
-    #     if (start_time < '09:00' || start_time > '18:00') && (end_time >= '18:01')
-    #         errors.add(:base, 'Out of opening hours')
-    #         throw :abort
-    #     elsif ('12:00..12:59').cover?(start_time)
-    #         errors.add(:base, 'Out of opening hours')
-    #         throw :abort
-    #     end
-    # end
+    def valid_date
+        if ends_at < starts_at
+            errors.add(:base, 'Invalid date')
+        end
+    end
 end
